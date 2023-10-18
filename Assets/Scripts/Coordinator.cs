984a38f03cad9bf783138110f33b42eab2f4f286
Frank Taylor <frank.taylor.pcg@gmail.com>
2023-10-18 12:42:23 -0500

    Adding MQTT, node-red, and instructions

---
+++ Assets/Scripts/Coordinator.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MQTTnet.Client;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+public class Coordinator : MonoBehaviour
+{
+	private const string MqttClientAlreadyConnected = "MQTT client already connected";
+	private const string MqttClientFailedToInitialize = "Failed to initialize MQTT client";
+	private const string MqttClientFailedToConnect = "Failed to connect to MQTT broker";
+	private const string MqttClientSetUpSuccessfully = "MQTT client set up successfully";
+	
+	ManagedMqttClient _client = new ManagedMqttClient();
+	
+	private readonly List<string> _subscribeTopics = new()
+	{
+		"unity/machine",
+	};
+	
+	// TODO: There HAS to be a better way to do this -- some kind of NamedEvent class, but how can I make that editable in the Inspector window?
+	public UnityEvent HomeEvents;
+	public UnityEvent TeachPoint1Events;
+	public UnityEvent TeachPoint2Events;
+	public UnityEvent MaxExtentEvents;
+	
+	private string topic = null;
+	private string message = null;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		print(SetupMqtt());
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (message == null) return;
+		
+		switch (message)
+		{
+			case "Home":
+			{
+				print("Move to Home");
+				HomeEvents.Invoke();
+				break;
+			}
+			case "TeachPoint1":
+			{
+				print("Move to teachpoint 1");
+				TeachPoint1Events.Invoke();
+				break;
+			}
+			case "TeachPoint2":
+			{
+				print("Move to teachpoint 2");
+				TeachPoint2Events.Invoke();
+				break;
+			}
+			case "MaxExtent":
+			{
+				print("Move to max extents");
+				MaxExtentEvents.Invoke();
+				break;
+			}
+			default:
+			{
+				print($"Received: {message} from {topic}");
+				break;
+			}
+		}
+		// Reset the message so we don't keep firing the events
+		message = null;
+	}
+	
+	// These allow the UI to move the stages through the coordinator
+	public void MoveToMaxExtents() => MaxExtentEvents.Invoke();
+	public void MoveToTeachPoint1() => TeachPoint1Events.Invoke();
+	public void MoveToTeachPoint2() => TeachPoint2Events.Invoke();
+	public void MoveToHome() => HomeEvents.Invoke();
+	
+	// Handles the MQTT client setup and returns a message indicating the success or failure
+	private string SetupMqtt()
+	{
+		if (_client.IsConnected) return MqttClientAlreadyConnected;
+
+		if (!_client.Initialize()) return MqttClientFailedToInitialize;
+
+		_client.ConnectWithTimeout().Wait();
+		if (!_client.IsConnected) return MqttClientFailedToConnect;
+
+		_client.SetMessageHandler(HandleMessage);
+		_subscribeTopics.ForEach(_client.Subscribe);
+		
+		return MqttClientSetUpSuccessfully;		
+	}
+	
+	private Task HandleMessage(MqttApplicationMessageReceivedEventArgs e)
+	{
+		message = Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment.Array);
+		string topic = e.ApplicationMessage.Topic;
+		
+		return Task.CompletedTask;
+	}
+}
---
+++ Assets/Scripts/ManagedMqttClient.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+
+public class ManagedMqttClient
+{
+	private MqttFactory? _factory;
+	private IMqttClient? _client;
+	private MqttClientOptions? _options;
+
+	public bool Initialize()
+	{
+		_factory = new MqttFactory();
+		_client = _factory.CreateMqttClient();
+		_options = new MqttClientOptionsBuilder()
+			.WithTcpServer("127.0.0.1")
+			.Build();
+
+		return _factory is not null && _client is not null && _options is not null;
+	}
+
+	public bool IsConnected => _client?.IsConnected ?? false;
+
+	public Task<MqttClientConnectResult> ConnectWithTimeout(int milliseconds = 250)
+	{
+		// How do I cleanly exit this and report a failure?
+		if (_client is null || _options is null)
+			return Task.FromResult(new MqttClientConnectResult());
+
+		try
+		{
+			using CancellationTokenSource timeoutToken = new(TimeSpan.FromMilliseconds(milliseconds));
+			return _client.ConnectAsync(_options, timeoutToken.Token);
+		}
+			catch (OperationCanceledException)
+			{
+				Console.WriteLine("Timeout when connecting to MQTT broker");
+			}
+
+		return Task.FromResult(new MqttClientConnectResult());
+	}
+
+	public void SetMessageHandler(Func<MqttApplicationMessageReceivedEventArgs, Task> del)
+	{
+		if (_client is null) return;
+
+		_client.ApplicationMessageReceivedAsync += del;
+	}
+
+	public async void Publish(string topic, string msg)
+	{
+		if (!IsConnected)
+		{
+			Console.WriteLine("Can't publish a message because the client is not connected!");
+			return;
+		}
+
+		MqttApplicationMessage? message = new MqttApplicationMessageBuilder()
+			.WithTopic(topic)
+			.WithPayload(msg)
+			.WithQualityOfServiceLevel(MqttQualityOfServiceLevel.ExactlyOnce)
+			.Build();
+
+		await _client!.PublishAsync(message, CancellationToken.None);
+	}
+
+	public async void Subscribe(string topic)
+	{
+		if (_factory is null) return;
+
+		MqttClientSubscribeOptions? subscribeOptions = _factory!
+			.CreateSubscribeOptionsBuilder()
+			.WithTopicFilter(x => x.WithTopic(topic))
+			.Build();
+
+		Task<MqttClientSubscribeResult> result = _client!.SubscribeAsync(subscribeOptions, CancellationToken.None);
+
+		await result;
+
+		Console.WriteLine($"MqttClient successfully subscribed to the [{topic}] topic");
+	}
+}
